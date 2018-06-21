c9b1a6299884b11cc408afc5bb7e09edcc3e5996
masterquantumfin <40475401+masterquantumfin@users.noreply.github.com>
2018-06-22 00:11:10 +0200

    Update issue.js

--- test/issue.js
+++ test/issue.js
@@ -6,7 +6,7 @@ const zapier = require('zapier-platform-core');
 const App = require('../index');
 const appTester = zapier.createAppTester(App);
 
-const REPO = 'username/reponame' // CHANGE THIS
+const REPO = 'masterquantumfin/prova2' // CHANGE THIS
 
 //These are automated tests for the Issue create and Issue Trigger.
 //They will run every time the `zapier test` command is executed.
